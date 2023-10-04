50b2be03b5c9e1b83dc34fc04b409510a435a6e5
GnagetEmil <emil@gnaget.se>
2023-10-04 22:53:15 +0200

    Added navcontroller for signin/signup and signin view UI

---
+++ app/src/main/java/com/example/medlemma/View/SignInScreen.kt
@@ -0,0 +1,82 @@
+package com.example.medlemma.View
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun SignInScreen(navController: NavController, signUpAction: (email: String, pass: String) -> Unit) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val keyboardController = LocalSoftwareKeyboardController.current
+
+        Text(text = "Sign Up", fontSize = 24.sp, modifier = Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.height(24.dp))
+
+        val emailState = rememberSaveable { mutableStateOf("") }
+        val passwordState = rememberSaveable { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = emailState.value,
+            onValueChange = { emailState.value = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() })
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = passwordState.value,
+            onValueChange = { passwordState.value = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() })
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Button(onClick = {
+            signUpAction(emailState.value, passwordState.value)
+        }) {
+            Text("Sign Up")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+        TextButton(onClick = {
+            navController.navigate("signUp") // assuming this is the route name for SignIn screen
+        }) {
+            Text("Dont have an account? Sign up")
+        }
+    }
+}
