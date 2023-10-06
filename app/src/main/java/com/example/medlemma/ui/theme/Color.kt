438e15447d295901f23bc0dd1056bef5044bb8d7
ludde94 <l.jenisch@hotmail.com>
2023-10-06 11:46:06 +0200

    Added the right colorcodes

--- app/src/main/java/com/example/medlemma/ui/theme/Color.kt
+++ app/src/main/java/com/example/medlemma/ui/theme/Color.kt
@@ -3,12 +3,12 @@ package com.example.medlemma.ui.theme
 import androidx.compose.ui.graphics.Color
 
 //background
-val LightGray = Color(0xFFF51818) // Light gray background
+val LightGray = Color(0xFFD9D9D9) // Light gray background
 
 //font colors
-val RegularFontColor = Color(0xFF333333) // Regular font color (nearly black)
+val RegularFontColor = Color(0xFF111010) // Regular font color (nearly black)
 val HeadlineFontColor = Color(0xFF000000) // Headline font color (black)
 
 
 //buttons
-val ButtonColor = Color(0xF5F5F5) // Slightly darker button color
+val ButtonColor = Color(0xFF353736) // Slightly darker button color
