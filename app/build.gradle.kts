cf7a9e35992b24624e7a68bca5fdb22527e5ec21
Gabriel Petersson <gabriel@littleland.se>
2023-10-06 16:50:59 +0200

    Updated the SignUp design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
@@ -65,7 +64,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.compose.runtime:runtime-livedata:<compose_version>")
-
+    implementation("androidx.compose.material:material-icons-core:1.5.3")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -74,4 +73,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
 }
--- app/src/main/java/com/example/medlemma/View/SignUpScreen.kt
+++ app/src/main/java/com/example/medlemma/View/SignUpScreen.kt
@@ -1,21 +1,30 @@
 package com.example.medlemma.View
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -27,17 +36,25 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.medlemma.R
 import com.example.medlemma.ViewModel.SignupViewModel
 import com.example.medlemma.ui.theme.Blue
 
+val customShape = Shapes(
+    small = RoundedCornerShape(10.dp), // Adjust the radius as needed
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(40.dp)
+)
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpScreen(navController: NavController, viewModel: SignupViewModel, signUpAction: (email: String, pass: String, confirmPass: String) -> Unit) {
@@ -68,7 +85,8 @@ fun SignUpScreen(navController: NavController, viewModel: SignupViewModel, signU
                 label = { Text("Email") },
                 modifier = Modifier.fillMaxWidth(),
                 keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
-                keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() })
+                keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() }),
+                shape = customShape.large
             )
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -79,7 +97,8 @@ fun SignUpScreen(navController: NavController, viewModel: SignupViewModel, signU
                 modifier = Modifier.fillMaxWidth(),
                 keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
                 keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() }),
-                visualTransformation = PasswordVisualTransformation()
+                visualTransformation = PasswordVisualTransformation(),
+                shape = customShape.large
             )
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -90,17 +109,49 @@ fun SignUpScreen(navController: NavController, viewModel: SignupViewModel, signU
                 modifier = Modifier.fillMaxWidth(),
                 keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
                 keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() }),
-                visualTransformation = PasswordVisualTransformation()
+                visualTransformation = PasswordVisualTransformation(),
+                shape = customShape.large
             )
             Spacer(modifier = Modifier.height(26.dp))
 
-            Button(modifier = Modifier.align(Alignment.End), onClick = {
+            Button(
+                modifier = Modifier.width(220.dp),
+                contentPadding = PaddingValues(vertical = 10.dp, horizontal = 20.dp),
+                onClick = {
                 signUpAction(emailState.value, passwordState.value, confirmPassState.value)
             }) {
                 Text("Sign Up")
             }
 
+            Spacer(modifier = Modifier.height(100.dp))
+
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.width(220.dp),
+                contentPadding = PaddingValues(vertical = 10.dp, horizontal = 20.dp)
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(painter = painterResource(id = R.drawable.icons8_google), contentDescription = "", modifier = Modifier.size(24.dp))
+                    Text("  Sign up with Google", color = Color.White)
+                }
+            }
+
             Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.width(220.dp),
+                contentPadding = PaddingValues(vertical = 10.dp, horizontal = 20.dp)
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(painter = painterResource(id = R.drawable.ic_facebook), contentDescription = "")
+                    Text("  Sign up with Facebook", color = Color.White)
+                }
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
         }
 
         Box(
@@ -131,10 +182,32 @@ fun SignUpScreen(navController: NavController, viewModel: SignupViewModel, signU
                     modifier = Modifier
                         .align(Alignment.BottomCenter)
                         .padding(8.dp)
-                        .padding(bottom = 56.dp) // Ensure this is above the "Sign In" text.
+                        .padding(bottom = 220.dp) // Ensure this is above the "Sign In" text.
                 )
             }
         }
 
+    }
+}
+
+@Composable
+fun SocialMediaButton(
+    text: String,
+    icon: ImageVector,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(imageVector = icon, contentDescription = null)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = text)
+        }
     }
 }
