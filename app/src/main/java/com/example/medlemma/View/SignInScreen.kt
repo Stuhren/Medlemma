9099f4a4bb5f59f5844c014c5f7a4b1d2ed3ee53
GnagetEmil <emil@gnaget.se>
2023-10-06 14:35:37 +0200

    Design changes.

--- app/src/main/java/com/example/medlemma/View/SignInScreen.kt
+++ app/src/main/java/com/example/medlemma/View/SignInScreen.kt
@@ -1,107 +1,112 @@
 package com.example.medlemma.View
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.medlemma.ViewModel.SigninViewModel
+import com.example.medlemma.ui.theme.Blue
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun SignInScreen(navController: NavController, viewModel: SigninViewModel, signUpAction: (email: String, pass: String) -> Unit) {
-    val context = LocalContext.current
-
-    // Retrieves the login error message from the SigninViewModel
+fun SignInScreen(navController: NavController, viewModel: SigninViewModel, signInAction: (email: String, pass: String) -> Unit) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     val errorMessage by viewModel.errorMessage.observeAsState()
-
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        val keyboardController = LocalSoftwareKeyboardController.current
-
-        Text(text = "Sign in", fontSize = 24.sp, modifier = Modifier.align(Alignment.CenterHorizontally))
-        Spacer(modifier = Modifier.height(24.dp))
-
-        val emailState = rememberSaveable { mutableStateOf("") }
-        val passwordState = rememberSaveable { mutableStateOf("") }
-
-        OutlinedTextField(
-            value = emailState.value,
-            onValueChange = { emailState.value = it },
-            label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
-            keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() })
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-
-        OutlinedTextField(
-            value = passwordState.value,
-            onValueChange = { passwordState.value = it },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
-            keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() }),
-            visualTransformation = PasswordVisualTransformation()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-
-
-        Button(onClick = {
-            signUpAction(emailState.value, passwordState.value)
-        }) {
-            Text("Sign Up")
-        }
-
-        Spacer(modifier = Modifier.height(24.dp))
-        TextButton(onClick = {
-            navController.navigate("signUp") // assuming this is the route name for SignIn screen
-        }) {
-            Text("Dont have an account? Sign up")
-        }
-    }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(bottom = 40.dp), // Fills the entire available space
-        contentAlignment = Alignment.BottomCenter// Aligns content inside the box to the bottom
-    ){
-        if (!errorMessage.isNullOrEmpty()) {
-            Text(
-                text = errorMessage ?: "",
-                color = Color.Red,
-                modifier = Modifier.padding(8.dp)
+            .padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Login", fontSize = 34.sp, modifier = Modifier.align(Alignment.Start))
+            Text(text = "Please sign in to continue.", fontSize = 16.sp, modifier = Modifier.align(Alignment.Start))
+            Spacer(modifier = Modifier.height(24.dp))
+
+            val emailState = rememberSaveable { mutableStateOf("") }
+            val passwordState = rememberSaveable { mutableStateOf("") }
+
+            OutlinedTextField(
+                value = emailState.value,
+                onValueChange = { emailState.value = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+                keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() })
             )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = passwordState.value,
+                onValueChange = { passwordState.value = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+                keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() }),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Spacer(modifier = Modifier.height(26.dp))
+
+            Button(modifier = Modifier.align(Alignment.End), onClick = {
+                signInAction(emailState.value, passwordState.value)
+            }) {
+                Text("LOGIN")
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
         }
+
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text("Don't have an account? ")
+
+                val signUpText = "Sign up"
+                val signUpString = AnnotatedString(signUpText)
+
+                ClickableText(
+                    text = signUpString,
+                    style = MaterialTheme.typography.bodyLarge.copy(color = Blue),
+                    onClick = {
+                        navController.navigate("signUp")
+                    }
+                )
+            }
+
+            if (!errorMessage.isNullOrEmpty()) {
+                Text(
+                    text = errorMessage ?: "",
+                    color = Color.Red,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(8.dp)
+                        .padding(bottom = 56.dp) // Ensure this is above the "Sign up" text.
+                )
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/medlemma/View/SignUpScreen.kt
+++ app/src/main/java/com/example/medlemma/View/SignUpScreen.kt
@@ -3,15 +3,18 @@ package com.example.medlemma.View
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -26,94 +29,112 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.medlemma.ViewModel.SignupViewModel
+import com.example.medlemma.ui.theme.Blue
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpScreen(navController: NavController, viewModel: SignupViewModel, signUpAction: (email: String, pass: String, confirmPass: String) -> Unit) {
-    val context = LocalContext.current
-    // Observe the signup error message
+    val keyboardController = LocalSoftwareKeyboardController.current
     val errorMessage by viewModel.signupErrorMessage.observeAsState()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        val keyboardController = LocalSoftwareKeyboardController.current
-
-        Text(text = "Sign Up", fontSize = 24.sp, modifier = Modifier.align(Alignment.CenterHorizontally))
-        Spacer(modifier = Modifier.height(24.dp))
-
-        val emailState = rememberSaveable { mutableStateOf("") }
-        val passwordState = rememberSaveable { mutableStateOf("") }
-        val confirmPassState = rememberSaveable { mutableStateOf("") }
-
-        OutlinedTextField(
-            value = emailState.value,
-            onValueChange = { emailState.value = it },
-            label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
-            keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() })
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-
-        OutlinedTextField(
-            value = passwordState.value,
-            onValueChange = { passwordState.value = it },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
-            keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() }),
-            visualTransformation = PasswordVisualTransformation()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-
-        OutlinedTextField(
-            value = confirmPassState.value,
-            onValueChange = { confirmPassState.value = it },
-            label = { Text("Confirm Password") },
-            modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
-            keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
-            visualTransformation = PasswordVisualTransformation()
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Button(onClick = {
-            signUpAction(emailState.value, passwordState.value, confirmPassState.value)
-        }) {
-            Text("Sign Up")
-        }
-
-
-        Spacer(modifier = Modifier.height(24.dp))
-        TextButton(onClick = {
-            navController.navigate("signIn") // assuming this is the route name for SignIn screen
-        }) {
-            Text("Already have an account? Sign In")
-        }
-    }
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(bottom = 40.dp), // Fills the entire available space
-        contentAlignment = Alignment.BottomCenter// Aligns content inside the box to the bottom
-    ){
-        if (!errorMessage.isNullOrEmpty()) {
-            Text(
-                text = errorMessage ?: "",
-                color = Color.Red,
-                modifier = Modifier.padding(8.dp)
+            .padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Sign Up", fontSize = 34.sp, modifier = Modifier.align(Alignment.Start))
+            Text(text = "Please register to continue.", fontSize = 16.sp, modifier = Modifier.align(Alignment.Start))
+            Spacer(modifier = Modifier.height(24.dp))
+
+            val emailState = rememberSaveable { mutableStateOf("") }
+            val passwordState = rememberSaveable { mutableStateOf("") }
+            val confirmPassState = rememberSaveable { mutableStateOf("") }
+
+            OutlinedTextField(
+                value = emailState.value,
+                onValueChange = { emailState.value = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+                keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() })
             )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = passwordState.value,
+                onValueChange = { passwordState.value = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+                keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() }),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = confirmPassState.value,
+                onValueChange = { confirmPassState.value = it },
+                label = { Text("Confirm Password") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+                keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() }),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Spacer(modifier = Modifier.height(26.dp))
+
+            Button(modifier = Modifier.align(Alignment.End), onClick = {
+                signUpAction(emailState.value, passwordState.value, confirmPassState.value)
+            }) {
+                Text("Sign Up")
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
         }
+
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text("Already have an account? ")
+
+                val signInText = "Sign In"
+                val signInString = AnnotatedString(signInText)
+
+                ClickableText(
+                    text = signInString,
+                    style = MaterialTheme.typography.bodyLarge.copy(color = Blue),
+                    onClick = {
+                        navController.navigate("signIn")
+                    }
+                )
+            }
+
+            if (!errorMessage.isNullOrEmpty()) {
+                Text(
+                    text = errorMessage ?: "",
+                    color = Color.Red,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(8.dp)
+                        .padding(bottom = 56.dp) // Ensure this is above the "Sign In" text.
+                )
+            }
+        }
+
     }
 }
