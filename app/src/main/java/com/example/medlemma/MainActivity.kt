d78eae1b3f92e3179ef51b85b811d9db1b1b60eb
Stuhren <you@example.com>
2023-10-09 16:38:29 +0200

    Added navigation bar with a menu.

--- app/src/main/java/com/example/medlemma/MainActivity.kt
+++ app/src/main/java/com/example/medlemma/MainActivity.kt
@@ -1,31 +1,41 @@
 package com.example.medlemma
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.medlemma.ui.theme.MedlemmaTheme
 import com.example.medlemma.View.SignUpScreen
 import com.example.medlemma.View.SignInScreen
-import com.example.medlemma.ViewModel.SignupViewModel
-import com.example.medlemma.ViewModel.SigninViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.example.medlemma.View.AdminDashboard
 import com.example.medlemma.View.BrowseMemberships
+import com.example.medlemma.View.DrawerBody
+import com.example.medlemma.View.DrawerHeader
 import com.example.medlemma.View.MyMemberships
+import com.example.medlemma.ViewModel.AppBar
+import com.example.medlemma.ViewModel.MenuItem
 import com.example.medlemma.ViewModel.MyMembershipsViewModel
-
+import com.example.medlemma.ViewModel.SigninViewModel
+import com.example.medlemma.ViewModel.SignupViewModel
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private lateinit var signUpViewModel: SignupViewModel
     private lateinit var signInViewModel: SigninViewModel
     private lateinit var myMembershipsViewModel: MyMembershipsViewModel
+
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -34,16 +44,46 @@ class MainActivity : ComponentActivity() {
         myMembershipsViewModel = ViewModelProvider(this).get(MyMembershipsViewModel::class.java)
 
         setContent {
+            val navController = rememberNavController()
             MedlemmaTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                val scaffoldState = rememberScaffoldState()
+                val scope = rememberCoroutineScope()
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                        AppBar(
+                            onNavigationIconClick = {
+                                scope.launch {
+                                    scaffoldState.drawerState.open()
+                                }
+                            }
+                        )
+                    },
+                    drawerContent = {
+                        DrawerHeader()
+                        DrawerBody(items = listOf(
+                            MenuItem(
+                                id = "myMemberships",
+                                title = "My Memberships",
+                                contentDescription = "My Memberships",
+                                icon = Icons.Default.Home
+                            ),
+                            MenuItem(
+                                id = "browseMemberships",
+                                title = "Browse Memberships",
+                                contentDescription = "Browse Memberships",
+                                icon = Icons.Default.Home
+                            )
+                        ), onItemClick = {
+                            navController.navigate(it.id)
+                        })
+                    }
                 ) {
-                    val navController = rememberNavController()
+                    // Place your NavHost inside the Scaffold content
                     NavHost(navController, startDestination = "signIn") {
                         composable("signIn") {
                             SignInScreen(navController, signInViewModel) { email, pass ->
-                                signInViewModel.signIn(navController,email, pass)
+                                signInViewModel.signIn(navController, email, pass)
                             }
                         }
                         composable("signUp") {
@@ -60,13 +100,9 @@ class MainActivity : ComponentActivity() {
                         composable("myMemberships") {
                             MyMemberships()
                         }
-
-
-                        // Add other composables/screens as needed.
                     }
                 }
             }
         }
-
     }
 }
---
+++ app/src/main/java/com/example/medlemma/ViewModel/AppBar.kt
@@ -0,0 +1,43 @@
+package com.example.medlemma.ViewModel
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.medlemma.R
+
+@ExperimentalMaterial3Api
+@Composable
+fun AppBar(
+    onNavigationIconClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+    ) {
+        TopAppBar(
+            title = {
+                Text(text = "Medlemma",
+                    style = MaterialTheme.typography.titleSmall.copy(color = MaterialTheme.colorScheme.onPrimary)
+                    )
+            },
+            navigationIcon = {
+                IconButton(onClick = onNavigationIconClick) {
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = "Toggle drawer menu"
+                    )
+                }
+            }
+        )
+    }
+}
