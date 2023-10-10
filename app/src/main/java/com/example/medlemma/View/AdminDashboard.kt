dcc26e6a368f79f37057ca66ad72afb233accccd
GnagetEmil <emil@gnaget.se>
2023-10-10 10:22:41 +0200

    Add UI for Add company admin dashboard

--- app/src/main/java/com/example/medlemma/View/AdminDashboard.kt
+++ app/src/main/java/com/example/medlemma/View/AdminDashboard.kt
@@ -1,13 +1,26 @@
 package com.example.medlemma.View
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.medlemma.R
+import com.example.medlemma.ui.theme.SoftGray
 
 @Composable
 fun AdminDashboard() {
@@ -15,29 +28,75 @@ fun AdminDashboard() {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        GreetingText(message = "ADMIN DASHBOARD", from = "")
+        AddCompany()
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
-private fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+private fun AddCompany(modifier: Modifier = Modifier) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize().padding(start = 16.dp, end = 16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+        // Add company
+        Text(text = "Add Company", fontSize = 22.sp, modifier = Modifier.align(Alignment.Start).padding(top = 30.dp))
+        Spacer(modifier = Modifier.height(10.dp))
+        // Category
+
+        val categoryState = rememberSaveable { mutableStateOf("") }
+        OutlinedTextField(
+            value = categoryState.value,
+            onValueChange = { categoryState.value = it },
+            label = { Text("Category") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() })
         )
-        Text(
-            text = from,
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.End)
+        Spacer(modifier = Modifier.height(10.dp))
+        // Name
+
+        val nameState = rememberSaveable { mutableStateOf("") }
+        OutlinedTextField(
+            value = categoryState.value,
+            onValueChange = { categoryState.value = it },
+            label = { Text("Name") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() })
         )
+        Spacer(modifier = Modifier.height(10.dp))
+        // RegisterUrl
+
+        val registerurlState = rememberSaveable { mutableStateOf("") }
+        OutlinedTextField(
+            value = categoryState.value,
+            onValueChange = { categoryState.value = it },
+            label = { Text("Register Url") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() })
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(modifier = Modifier.align(Alignment.Start), onClick = {
+            //addCompanyLogoAction(categoryState.value, nameState.value, registerurlState.value)
+        }) {
+            Text("Upload Company Logo")
+        }
+
+
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(modifier = Modifier.align(Alignment.End), onClick = {
+            //addCompanyAction(categoryState.value, nameState.value, registerurlState.value)
+        }) {
+            Text("Add")
+        }
+
     }
 }
