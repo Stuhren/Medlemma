66ec62eb077b60455356859180ef503c752dff37
ludde94 <l.jenisch@hotmail.com>
2023-10-10 10:42:02 +0200

    Added cards around all the images for further ui development

--- app/src/main/java/com/example/medlemma/View/MyMemberships.kt
+++ app/src/main/java/com/example/medlemma/View/MyMemberships.kt
@@ -1,13 +1,19 @@
 package com.example.medlemma.View
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -19,6 +25,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberImagePainter
 import com.example.medlemma.ViewModel.MyMembershipsViewModel
+import com.example.medlemma.ui.theme.SoftGray
 
 @Composable
 fun MyMemberships() {
@@ -30,35 +37,67 @@ fun MyMemberships() {
     val logos by viewModel.fetchLogos().observeAsState(initial = emptyList())
 
     // The main composable
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp)
     ) {
-        Text(
-            text = "MY MEMBERSHIPS",
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
-        Text(
-            text = "Categories: ${categories.joinToString(", ")}",
-            fontSize = 36.sp,
-            modifier = Modifier.padding(16.dp).align(alignment = Alignment.End)
-        )
-
-        // Display logos
-        logos.forEach { logoUrl ->
-            Image(
-                painter = rememberImagePainter(data = logoUrl),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
+        item {
+            Box(
                 modifier = Modifier
-                    .size(150.dp)
-                    .clip(shape = MaterialTheme.shapes.medium)
-                    .align(Alignment.CenterHorizontally)
-                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
+            ) {
+                Text(
+                    text = "MY MEMBERSHIPS",
+                    fontSize = 36.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+            Text(
+                text = "Categories: ${categories.joinToString(", ")}",
+                fontSize = 20.sp,
+                modifier = Modifier.padding(bottom = 16.dp)
             )
+        }
+
+        // Display logos in clickable cards with a soft gray background and border
+        items(logos) { logoUrl ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clickable {
+                            // Handle card click here
+                            // You can navigate to a detailed view or perform other actions
+                        }
+                        .background(SoftGray) // Set the background color to SoftGray
+                        .clip(RoundedCornerShape(12.dp))
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .border(1.dp, SoftGray) // Add a border with SoftGray color
+                    ) {
+                        Image(
+                            painter = rememberImagePainter(data = logoUrl),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .height(225.dp) // Adjust the height as needed
+                        )
+                    }
+                }
+            }
         }
     }
 }
+
+
+
+
