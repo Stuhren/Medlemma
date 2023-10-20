a2461e661b060d2e86eb991fd52d4387cfcb0419
Fredrik Johnsson <fredrik.johnsson0029@stud.hkr.se>
2023-10-20 12:30:20 +0200

    Added search functionality to memberships

--- app/src/main/java/com/example/medlemma/View/BrowseMemberships.kt
+++ app/src/main/java/com/example/medlemma/View/BrowseMemberships.kt
@@ -1,7 +1,6 @@
 package com.example.medlemma.View
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -11,7 +10,6 @@ import androidx.compose.material3.Card
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -19,24 +17,39 @@ import coil.compose.rememberImagePainter
 import com.example.medlemma.ViewModel.MyMembershipsViewModel
 import com.example.medlemma.ui.theme.CustomShapes
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.livedata.observeAsState
-import com.example.medlemma.Model.CompanyModel
 import com.example.medlemma.ui.theme.DarkGray
 import com.example.medlemma.ui.theme.MedlemmaTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BrowseMemberships() {
     MedlemmaTheme {
         val viewModel: MyMembershipsViewModel = viewModel()
         val companies by viewModel.fetchAllCompanies().observeAsState(initial = emptyList())
 
+        var searchQuery by remember { mutableStateOf("") }
+
         Box(modifier = Modifier.fillMaxSize()) {
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(16.dp)
+            Column(
+                modifier = Modifier.fillMaxSize()
             ) {
-                // Display all companies
-                items(companies) { item ->
+                OutlinedTextField(
+                    value = searchQuery,
+                    onValueChange = {searchQuery = it},
+                    label = { Text(text = "Search Memberships")},
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                val filteredCompanies = companies.filter {
+                    it.companyName.contains(searchQuery, ignoreCase = true)
+                }
+
+                LazyColumn {
+                items(filteredCompanies) { item ->
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
@@ -46,7 +59,6 @@ fun BrowseMemberships() {
                             modifier = Modifier
                                 .fillMaxSize()
                                 .clickable {
-                                    // Handle click on a company if needed
                                 }
                                 .clip(shape = CustomShapes.medium)
                         ) {
@@ -57,7 +69,7 @@ fun BrowseMemberships() {
                                         4.dp,
                                         DarkGray,
                                         shape = CustomShapes.medium
-                                    ) // Add a border with SoftGray color
+                                    )
                             ) {
                                 Image(
                                     painter = rememberImagePainter(data = item.companyLogo),
@@ -65,8 +77,9 @@ fun BrowseMemberships() {
                                     contentScale = ContentScale.Crop,
                                     modifier = Modifier
                                         .fillMaxSize()
-                                        .height(220.dp) // Adjust the height as needed
+                                        .height(220.dp)
                                 )
+                            }
                             }
                         }
                     }
